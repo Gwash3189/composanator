849f529a0f6bed36c76063752095417bf0543aa8
Adam Beck <adamb3189@gmail.com>
2015-05-20 13:52:44 +1000

    update api to include left and right apis.

--- composanator.js
+++ composanator.js
@@ -2,32 +2,63 @@ var namespace = this.window === undefined ?
     (this.module !== undefined && this.module.exports !== undefined ? 
         module.exports : 
         undefined) :
-    window
+    window;
 
 namespace.compose = (function(){
     "use strict";
+  
+    var directionEnum ={
+      "left": "left",
+      "right": "right"
+    };
+  
+    var forEach = function(direction, arr, func){
+      if(direction === directionEnum.left){
+        return reverseForEach(arr, func);
+      } else if(direction === directionEnum.right) {
+        normalForEach(arr, func);
+      }
+    };
+  
+    var normalForEach = function(arr, fun){
+      arr.forEach(fun);
+    };
+  
     var reverseForEach = function(arr, fun) {
-        var i = arr.length - 1
+        var i = arr.length - 1;
         for (i; i > -1; --i) {
             fun(arr[i], i, arr);
         }
-    }
- 
-    var compose = function() {
-        var funcs = Object.keys(arguments);
-        var outterArgs = arguments;
-        return function() {
-            var value;
-            var innerArgs = arguments;
-            reverseForEach(funcs, function(funcName) {
-                if (value !== undefined) {
-                    value = outterArgs[funcName].call(null, value);
-                } else {
-                    value = outterArgs[funcName].apply(null, innerArgs);
-                }
-            });
-            return value;
-        };
     };
-    return compose;
-})()
+  
+    var compose = function(direction) {
+        return function() {
+          var funcs = Object.keys(arguments);
+          var outterArgs = arguments;
+          return function() {
+              var value;
+              var innerArgs = arguments;
+              var loopFunc = function(funcName){
+                if (value !== undefined) {
+                      value = outterArgs[funcName].call(null, value);
+                  } else {
+                      value = outterArgs[funcName].apply(null, innerArgs);
+                  }
+              };
+              forEach(direction, funcs, loopFunc);
+              return value;
+          };
+        };
+        
+    };
+ 
+    var api = {
+      right: function(){
+        return compose(directionEnum.right).apply(null, arguments);
+      },
+      left: function() {
+        return compose(directionEnum.left).apply(null, arguments);
+      }
+    };
+    return api;
+})();
