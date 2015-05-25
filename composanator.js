eb1819a0e99039adbf922d3ae92d5ea820d5815f
Adam Beck <adam.beck@inlogik.com>
2015-05-26 09:10:36 +1000

    ditch browser compatibility

--- composanator.js
+++ composanator.js
@@ -1,67 +1,58 @@
-var namespace;
-try {
-    if(module && module.exports){
-        namespace = module.exports;
-    }
-} catch(e) {
-    namespace = (window.composanator = {});
-}
+"use strict";
 
-namespace.compose = (function(){
-    "use strict";
+var directionEnum = {
+    "left": "left",
+    "right": "right"
+};
 
-    var directionEnum ={
-      "left": "left",
-      "right": "right"
-    };
-
-    var forEach = function(direction, arr, func){
-      if(direction === directionEnum.left){
+var forEach = function(direction, arr, func) {
+    if (direction === directionEnum.left) {
         return normalForEach(arr, func);
-      } else if(direction === directionEnum.right) {
+    } else if (direction === directionEnum.right) {
         return reverseForEach(arr, func);
-      }
-    };
+    }
+};
 
-    var normalForEach = function(arr, fun){
-      arr.forEach(fun);
-    };
+var normalForEach = function(arr, fun) {
+    arr.forEach(fun);
+};
 
-    var reverseForEach = function(arr, fun) {
-        var i = arr.length - 1;
-        for (i; i > -1; --i) {
-            fun(arr[i], i, arr);
-        }
-    };
+var reverseForEach = function(arr, fun) {
+    var i = arr.length - 1;
+    for (i; i > -1; --i) {
+        fun(arr[i], i, arr);
+    }
+};
 
-    var compose = function(direction) {
+var compose = function(direction) {
+    return function() {
+        var funcs = Object.keys(arguments);
+        var outterArgs = arguments;
         return function() {
-          var funcs = Object.keys(arguments);
-          var outterArgs = arguments;
-          return function() {
-              var value;
-              var innerArgs = arguments;
-              var loopFunc = function(funcName){
+            var value;
+            var innerArgs = arguments;
+            var loopFunc = function(funcName) {
                 if (value !== undefined) {
-                      value = outterArgs[funcName].call(null, value);
-                  } else {
-                      value = outterArgs[funcName].apply(null, innerArgs);
-                  }
-              };
-              forEach(direction, funcs, loopFunc);
-              return value;
-          };
+                    value = outterArgs[funcName].call(null, value);
+                } else {
+                    value = outterArgs[funcName].apply(null, innerArgs);
+                }
+            };
+            forEach(direction, funcs, loopFunc);
+            return value;
         };
-
     };
 
-    var api = {
-      right: function(){
-        return compose(directionEnum.right).apply(null, arguments);
-      },
-      left: function() {
-        return compose(directionEnum.left).apply(null, arguments);
-      }
-    };
-    return api;
-})();
+};
+
+var api = {
+    right: function() {
+        return compose(directionEnum.right)
+            .apply(null, arguments);
+    },
+    left: function() {
+        return compose(directionEnum.left)
+            .apply(null, arguments);
+    }
+};
+module.exports = api;
