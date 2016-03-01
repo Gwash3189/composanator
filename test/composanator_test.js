90146ea810d1f6064502bcdf2aa2017aa0906374
Adam Beck <abeck@zendesk.com>
2016-03-01 12:01:08 +1100

    not so cryptic tests

--- test/composanator_test.js
+++ test/composanator_test.js
@@ -4,19 +4,20 @@ import { right, left } from '../src/composanator';
 import { expect } from 'chai';
 
 describe('composanator', function () {
-  const timeBy2 = (x) => 2 * x;
-  const divideBy2 = (x) => x / 2;
+  const leftConcat = (x) => `${x}left`;
+  const rightConcat = (x) => `${x}right`;
+
   describe('right', function () {
     it('composes the functions from right to left', () => {
-      expect(right(timeBy2, divideBy2)(1))
-        .to.equal(1);
+      expect(right(leftConcat, rightConcat)(''))
+        .to.equal('rightleft');
     });
   });
 
   describe('left', function () {
     it('composes the functions from left to right', () => {
-      expect(left(timeBy2, divideBy2)(2))
-        .to.equal(2);
+      expect(left(leftConcat, rightConcat)(''))
+        .to.equal('leftright');
     });
   });
 });
